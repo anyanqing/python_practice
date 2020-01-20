905f99494157e648532b312dbbf2603fb168af70
anyanqing <anyanqing@moseeker.com>
2020-01-20 17:55:07 +0800

    msg:文件作业和程序设计练习

--- python_course/W06/HomeWork.py
+++ python_course/W06/HomeWork.py
@@ -19,3 +19,50 @@
 # print(dict)
 # fo.close()
 
+
+# # 哈姆雷特词频统计
+# excludes = {'a', 'an', 'for', 'the', 'you', 'and', 'to', 'of', 'i', 'my', 'in', 'you', 'that', 'it', 'is', 'his',
+#             'not', 'with', 'this', 'your', 'but', 'as', 'be', 'he', 'what', 'have', 'o', 'will', 'so', 'me', 'do',
+#             'are', 'him', 'our', 'by', 'if', 'on', 'or', 's', 'd', 'by', 'thou', 'they', 'there', 'from', 'her',
+#             'how', 'at', 't', 'was', 'would', 'well', 'll', 'them', 'may', 'should', 'we'}
+#
+# special_characters = {'~', '!', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '-', '`', '{', '}', '[', ']',
+#                       ':', '"', '|', '\\', '\\n', '\\s', "'", ';', '<', '>', '?', ',', '.', '/'}
+#
+#
+# def getText(file):
+#     infile = open(file, 'r', encoding='utf-8').read().strip()
+#     infile_lower = infile.lower()
+#     for ch in special_characters:
+#         infile_lower = infile_lower.replace(ch, ' ')
+#     return infile_lower
+#
+#
+# def printWords(infile, num):
+#     words = infile.split()
+#     counts = {}
+#     for word in words:
+#         if word in excludes:
+#             continue
+#         counts[word] = counts.get(word, 0) + 1
+#
+#     items = list(counts.items())
+#     items.sort(key=lambda x: x[1], reverse=True)
+#     for i in range(num):
+#         word, count = items[i]
+#         print('{word:<10}{count:>5}'.format(word=word, count=count))
+#
+#
+# def main():
+#     hamletTxt = getText('D:\Repository\PycharmProjects\python_practice\python_course\W06\infiles\hamlet.txt')
+#     printWords(hamletTxt, 20)
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+# 中文分词学习
+import jieba
+print(jieba.lcut('中国是一个伟大的国家'))
+print(jieba.lcut("我是一条小青龙，小青龙"))
---
+++ python_course/W07/matchSim.py
@@ -0,0 +1,65 @@
+from random import *
+
+def printIntro():
+    print('This program simulates a game between two')
+    print('There are two players, A and B')
+    print('Probability(a number between 0 and 1) is used')
+
+
+def getInputs():
+    a = eval(input('What is the prob.player A wins?'))
+    b = eval(input('What is the prob.player B wins?'))
+    n = eval(input('How many games to simulate?'))
+    return a, b, n
+
+
+def simNGames(probA, probB, n):
+    winsA = 0
+    winsB = 0
+    for i in range(n):
+        scoreA, scoreB = simOneGame(probA, probB)
+        if scoreA > scoreB:
+            winsA += 1
+        else:
+            winsB += 1
+    return winsA, winsB
+
+
+def simOneGame(probA, probB):
+    scoreA = 0
+    scoreB = 0
+    serving = 'A'
+    while not gameOver(scoreA, scoreB):
+        if serving == 'A':
+            if random() < probA:
+                scoreA += 1
+            else:
+                serving = 'B'
+        else:
+            if random() < probB:
+                scoreB += 1
+            else:
+                serving = 'A'
+    return scoreA, scoreB
+
+
+def gameOver(a, b):
+    return a == 15 or b == 15
+
+
+def printSummary(winsA, winsB):
+    n = winsA + winsB
+    print('Games simulated: {n} times.'.format(n=n))
+    print('Wins for A: {winsA}({winsA_prob:0.1%})'.format(winsA=winsA, winsA_prob=winsA/n))
+    print('Wins for B: {winsB}({winsB_prob:0.1%})'.format(winsB=winsB, winsB_prob=winsB/n))
+
+
+def main():
+    printIntro()
+    probA, probB, n = getInputs()
+    winsA, winsB = simNGames(probA, probB, n)
+    printSummary(winsA, winsB)
+
+
+if __name__ == '__main__':
+    main()
